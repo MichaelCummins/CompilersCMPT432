df6a0e90ff9a7bdb2d190d69a3750ea4958b0063
MichaelCummins <Michael.Cummins1@marist.edu>
2019-02-22 15:25:01 -0500

    Translated most of the grammar into the parser

--- Projects/Parser.js
+++ Projects/Parser.js
@@ -7,7 +7,7 @@ var tree = new Tree();
 tree.addNode("Root","Branch");
 
 
-function parse(userInput){
+function parseStart(userInput){
     tokens = userInput;
     parseProgram();
 }
@@ -31,43 +31,81 @@ function parseStatementList(){
 }
 
 function parseStatement(){
-    
+    if(currentToken == "print"){
+        parsePrintStatement():
+    }else if(currentToken == "Assignment"){
+        parseAssignmentStatement();
+    }else if(currentToken == "VarDecl"){
+        parseVarDecl();
+    }else if(currentToken == "while"){
+        parseWhileStatement();
+    }else if(currentToken == "if"){
+        parseIfStatement();
+    }else{
+        parseBlock();
+    }
 }
 
 function parsePrintStatement(){
-    
+    matchAndConsume("print");
+    matchAndConsume("(");
+    parseExpr();
+    matchAndConsume(")");
 }
 
 function parseAssignmentStatement(){
-    
+    parseId();
+    matchAndConsume("=");
+    parseExpr();
 }
 
 function parseVarDecl(){
-    
+    parseType();
+    parseId();
 }
 
 function parseWhileStatement(){
-    
+    matchAndConsume("while");
+    parseBooleanExpr();
+    parseBlock();
 }
 
 function parseIfStatement(){
-    
+    matchAndConsume("if");
+    parseBooleanExpr;
+    parseBlock;
 }
 
 function parseExpr(){
-    
+    if(currentToken == digit???){
+       parseIntExpr();
+    }else if(currentToken == '"'){
+       parseStringExpr();
+    }else if(currentToken == "("){
+        parseBooleanExpr();
+    }else{
+        parseId();
+    }
 }
 
 function parseIntExpr(){
-    
+    parseDigit();
+    parseIntOP();
+    parseExpr();
 }
 
 function parseStringExpr(){
-    
+    matchAndConsume('"');
+    parseCharlist();
+    matchAndConsume('"');
 }
 
 function parseBooleanExpr(){
-    
+    matchAndConsume("(");
+    parseExpr();
+    parseBoolOP();
+    parseExpr();
+    matchAndConsume(")");
 }
 
 function parseId(){
@@ -91,30 +129,44 @@ function parseType(){
 }
 
 function parseChar(){
-    
+    matchAndConsume();
 }
 
 function parseSpace(){
-    
+    matchAndConsume(" ");
 }
 
 function parseDigit(){
-    
+    matchAndConsume();
 }
 
 function parseBoolOP(){
-    
+    if(currentToken == "="){
+        matchAndConsume("=");
+        matchAndConsume("=");
+    }else{
+        matchAndConsume("!");
+        matchAndConsume("=")
+    }
 }
 
 function parseBoolVal(){
-    
+    if(currentToken == "false"){
+        matchAndConsume("false");
+    }else{
+        matchAndConsume("true");
+    }
 }
 
 function parseIntOP(){
-    
+    matchAndConsume("+");
 }
     
     
-function matchAndConsume(Token){
-    
+function matchAndConsume(expectedToken){
+    returnValue = false;
+    if(currentToken == expectedToken){
+        returnValue = true;
+    }
+    return returnValue;
 }
