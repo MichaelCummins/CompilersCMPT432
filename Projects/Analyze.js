c3217cdfe00979e96e4a8da30bee87ab321c4577
MichaelCummins <Michael.Cummins1@marist.edu>
2019-04-01 22:36:47 -0400

    I figured I should start it

--- Projects/Analyze.js
+++ Projects/Analyze.js
@@ -41,23 +41,75 @@ function analyzerStart(userInput){
 }
 
 function analyzeProgram(){
+    ast.addNode("Program", "branch");
+    outputMessage("Analyzing program");
+    getNextAnalyzerToken();
+    analyzeBlock();
+    if(matchToken(currentAnalyzerToken, "EOF")){
+        getNextAnalyzerToken();
+    }
     
+    ast.endChildren();
 }
 
 function analyzeBlock(){
+    currentScope++;
+    outputMessage("Analyzing block");
     
+    analyzeStatementList();
+    
+    if(matchToken(currentAnalyzerToken, "R_Brace")){
+        getNextAnalyzerToken();
+    }
+    currentScope--;
+    ast.endChildren();
 }
 
 function analyzeStatementList(){
+    outputMessage("Analyzing Statement List");
     
+    if(matchToken(currentAnalyzerToken, "R_Brace")){
+        
+    }else if(matchToken(currentAnalyzerToken, "PRINT") || matchToken(currentAnalyzerToken, "ID")
+        || matchToken(currentAnalyzerToken, "INT")|| matchToken(currentAnalyzerToken, "STRING")
+        || matchToken(currentAnalyzerToken, "Boolean")|| matchToken(currentAnalyzerToken, "WHILE")
+        || matchToken(currentAnalyzerToken, "IF") || matchToken(currentAnalyzerToken, "L_Brace")){
+        analyzeStatement();
+        analyzeStatementList();
+    }
 }
 
 function analyzeStatement(){
-    
+    if(matchToken(currentAnalyzerToken, "PRINT")){
+        analyzePrintStatement();
+    }else if(matchToken(currentAnalyzerToken, "ID")){
+        analyzeAssignmentStatement();
+    }else if(matchToken(currentAnalyzerToken, "INT") || matchToken(currentAnalyzerToken, "STRING")
+            || matchToken(currentAnalyzerToken, "Boolean")){
+        analyzeVarDecl();
+    }else if(matchToken(currentAnalyzerToken, "WHILE")){
+        analyzeWhileStatement();
+    }else if(matchToken(currentAnalyzerToken, "IF")){
+        analyzeIfStatement();
+    }else if(matchToken(currentAnalyzerToken, "L_Brace")){
+        analyzeBlock();
+    }
 }
 
 function analyzePrintStatement(){
+    outputMessage("Analyzing print statement");
+    getNextAnalyzerToken();
     
+    if(matchToken(currentAnalyzerToken, "L_Paren")){
+        getNextAnalyzerToken();
+    }
+    analyzeExpr();
+    
+    if(matchToken(currentAnalyzerToken, "R_Paren")){
+        getNextAnalyzerToken();
+    }
+    
+    ast.endChildren();
 }
 
 function analyzeAssignmentStatement(){
@@ -73,11 +125,28 @@ function analyzeWhileStatement(){
 }
 
 function analyzeIfStatement(){
-    
+    outputMessage("Analyzing if statement");
+    getNextAnalyzerToken();
+    if(matchToken(currentAnalyzerToken, "L_Paren") || matchToken(currentAnalyzerToken, "Boolean")){
+        analyzeBooleanExpr();
+        getNextAnalyzerToken();
+        analyzeBlock();
+    }
+    ast.endChildren();
 }
 
 function analyzeExpr(){
+    outputMessage("Analyzing expression");
     
+    if(matchToken(currentAnalyzerToken, "DIGIT")){
+        analyzeIntExpr();
+    }else if(matchToken(currentAnalyzerToken, "Quote")){
+        analyzeStringExpr();
+    }else if(matchToken(currentAnalyzerToken, "L_Paren") || matchToken(currentAnalyzerToken, "Boolean")){
+        analyzeBooleanExpr();
+    }else if(matchToken(currentAnalyzerToken, "ID")){
+        
+    }
 }
 
 function analyzeIntExpr(){
