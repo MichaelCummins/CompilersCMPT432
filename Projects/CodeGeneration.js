6bae1b02ed6cf53793ee37a0cba16e956482ed5b
MichaelCummins <Michael.Cummins1@marist.edu>
2019-05-09 18:21:56 -0400

    Added tree traversal

--- Projects/CodeGeneration.js
+++ Projects/CodeGeneration.js
@@ -20,7 +20,41 @@ function codeGenStart(){
 }
 
 function traverseTree(position, depth){
-    
+    if(position.name == "root"){
+        codeGenRoot(position.children, depth);
+    }else if(position.name == "program"){
+        codeGenProgram(position.cildren, depth);
+    }else if(position.name == "block"){
+        codeGenBlock(position, depth);
+    }else if(position.name == "vardecl"){
+        codeGenVarDecl(position, depth);
+    }else if(position.name == "assignment"){
+        codeGenAssignment(position, depth);
+    }else if(position.name == "print"){
+        codeGenPrint(position, depth);
+    }else if(position.name == "if"){
+        codeGenIf(position, depth);
+    }else if(position.name == "while"){
+        codeGenWhile(position. depth);
+    }else if(position.name == "isEquals"){
+        codeGenIsEquals(position, depth);
+    }else if(position.name == "notEquals"){
+        codeGenNotEquals(position, depth);
+    }else if(position.name == "true" || position.name == "false"){
+        codeGenBoolean(position, depth);
+    }else if(position.kind == "charlist"){
+        codeGenString(position, depth);
+    }else if("abcdefghijklmnopqrstuvwxyz".includes(position.name)){
+        codeGenId(position, depth);
+    }else if("0123456789"){
+        codeGenDigit(position, depth);
+    }else if(position.name == "intop"){
+        return codeGenAddition(position, depth);
+    }else{
+        for(var i = 0; i < position.children.length; i++){
+            traverseTree(position.children[i], depth);
+        }
+    }
 }
 
 function codeGenRoot(){
